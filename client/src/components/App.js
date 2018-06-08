9580ec7e3f4c7c682524e330e27b5205650043ae
guane <Ethan.GuangxueCao@pega.com>
2018-06-08 11:02:02 +0000

    add components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return (
         <div>
-            Hello, world!
+            {/* BrowserRouter can have only 1 direct child*/} 
+            <BrowserRouter> 
+                <div>
+                    <Header/>
+                    <Route path="/" component={Landing} exact />
+                    <Route path="/surveys" component={Dashboard} exact />
+                    <Route path="/surveys/new" component={SurveyNew} exact/>
+                </div>
+            </BrowserRouter>
         </div>    
     );
 };
